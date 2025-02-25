fe1a9539e42d289c1a6c80e1f69522165f2a5505
ORGIsEvil <vasya.cherny.2006@mail.ru>
2025-02-25 19:46:06 +0500

    Furman, Bespalova add alllll

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+let field = [];
+let KREST = true;
+let steps = 0;
+let winnerKletki = [];
 
 const container = document.getElementById('fieldWrapper');
 
@@ -8,6 +12,7 @@ startGame();
 addResetListener();
 
 function startGame () {
+
     renderGrid(3);
 }
 
@@ -16,7 +21,9 @@ function renderGrid (dimension) {
 
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement('tr');
+        field[i] = []
         for (let j = 0; j < dimension; j++) {
+            field[i][j] = EMPTY;
             const cell = document.createElement('td');
             cell.textContent = EMPTY;
             cell.addEventListener('click', () => cellClickHandler(i, j));
@@ -27,13 +34,84 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    if (field[row][col] === EMPTY){
+        if (KREST) {
+            renderSymbolInCell(CROSS, row, col);
+            field[row][col] = CROSS
+        }
+        else {
+            renderSymbolInCell(ZERO, row, col);
+            field[row][col] = ZERO
+        }
+        let curr = field[row][col];
+
+        KREST = !KREST;
+        steps += 1;
+    }
+
+    console.log(winnerKletki);
+
+    whoWin();
+
+    let flag = false;
+    if (winnerKletki.length === 3) {
+        for (let kletka of winnerKletki) {
+            findCell(kletka[0], kletka[1]).style.color = "#900"
+        }
+        function fanc() { alert(`Победил CROSS`) }
+        setTimeout(fanc, 100)
+        flag = true;
+    }
+
+
+    if (flag === false && steps === field.length * field.length) {
+        function func() { alert("Победила дружба"); }
+        setTimeout(func, 100)
+    }
+
+
+    console.log(KREST);
     console.log(`Clicked on cell: ${row}, ${col}`);
 
+}
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function whoWin() {
+    if (field[0][0] === field[0][1] && field[0][1] === field[0][2]
+        && field[0][0] !== EMPTY) {
+        //return matrix[0][0];
+        winnerKletki=([0, 0], [0, 1], [0, 2]);
+    }
+    else if (field[1][0] === field[1][1] && field[1][1] === field[1][2]
+        && field[1][0] !== EMPTY) {
+        winnerKletki=[[1, 0], [1, 1], [1, 2]];
+    }
+    else if (field[2][0] === field[2][1] && field[2][1] === field[2][2]
+        && field[2][0] !== EMPTY) {
+        winnerKletki=[[2, 0], [2, 1], [2, 2]];
+    }
+
+    else if (field[0][0] === field[1][0] && field[1][0] === field[2][0]
+        && field[0][0] !== EMPTY) {
+        winnerKletki=[[0, 0], [1, 0], [2, 0]];
+    }
+    else if (field[0][1] === field[1][1] && field[1][1]  === field[2][1]
+        && field[1][1] !== EMPTY) {
+        winnerKletki=[[0, 1], [1, 1], [2, 1]];
+    }
+    else if (field[0][2] === field[1][2] && field[1][2] === field[2][2]
+        && field[2][2] !== EMPTY) {
+        winnerKletki=[[0, 2], [1, 2], [2, 2]];
+    }
+
+    else if (field[0][0] === field[1][1] && field[1][1] === field[2][2]
+        && field[2][2] !== EMPTY) {
+        winnerKletki=[[0, 0], [1, 1], [2, 2]];
+    }
+
+    else if (field[0][2] === field[1][1] && field[1][1] === field[2][0]
+        && field[0][2] !== EMPTY) {
+        winnerKletki=[[0, 2], [1, 1], [2, 0]];
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,7 +132,13 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
-    console.log('reset!');
+    const CROSS = 'X';
+    const ZERO = 'O';
+    const EMPTY = ' ';
+    let field = [];
+    let KREST = true;
+    let steps = 0;
+    let winnerKletki = [];
 }
 
 
